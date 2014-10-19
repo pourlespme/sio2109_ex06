35631ff7e08de6ad3852ebee237f865c10c5fff4
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-10-19 10:55:00 +0200

    update dr.txt

--- bin/ex06.dart
+++ bin/ex06.dart
@@ -1,25 +1,19 @@
 import 'functions_ex06.dart';
 
- //ex06 no. 1
-
 main() {
-	
-
+  //ex06 no. 1
 	var mots = ['Laval'];
 	var valide;
 	for (var mot in mots)
 		valide = palindrome(mot);
   print('Question 1: Le mot $mots est-il un palindrome? $valide');
   
-//No. 2
+  //No. 2
+  var today = new DateTime.now();
+  var anotherDay = new DateTime(2014, 1, 1);
+  differenceDates(today, anotherDay);
 
-var today = new DateTime.now();
-var anotherDay = new DateTime(2014, 1, 1);
-differenceDates(today, anotherDay);
-
-
-// no. 3
- 
+  // no. 3
   List noteEnChiffre = [96,91,85,78, 65, 59];
   var noteEnLettre;
   print('Question 3 : La conversion de notes numériques en lettres.');
@@ -28,30 +22,23 @@ differenceDates(today, anotherDay);
     print('Un pointage de $lettre = $noteEnLettre en notation');
   }
 
-  
- 
-	
-//No. 4  //Je ne trouve pas mon erreur pour les lignes 37 et 50... 
-  
- List classement = ['Mario', 'Michel', 'Emmanuel', 'Laurence', 'Marie', 'Alexandre', 'Catherine', 'Sophie', 'Ludovic'];
- print('Voici une liste de nom : $classement');
- List resultList = getThreeLists(classement);
- print('nom de moins de huit lettres:${resultList.elementAt(0).toString()}');
- print('nom de huit lettres:${resultList.elementAt(1).toString()}');
- print('nom de plus de huit lettres:${resultList.elementAt(2).toString()}');
- print('');
-
-  
+  //No. 4  //Je ne trouve pas mon erreur pour les lignes 37 et 50...   
+  List classement = ['Mario', 'Michel', 'Emmanuel', 'Laurence', 'Marie', 'Alexandre', 'Catherine', 'Sophie', 'Ludovic'];
+  print('Voici une liste de nom : $classement');
+  List resultList = getThreeLists(classement);  
+  print('nom de moins de huit lettres:${resultList.elementAt(0).toString()}');
+  print('nom de huit lettres:${resultList.elementAt(1).toString()}');
+  print('nom de plus de huit lettres:${resultList.elementAt(2).toString()}');
+  print('');
 
   // No. 5
-    List playersName = [['Malkin', 'Crosby'], ['Souray', 'Perry'], ['Karlsson', 'Phillips'], ['Johnson', 'Dubinsky'], ['Luongo', 'Hayes'], ['Tavares', 'Vanek']];
-    List clubsName = ['Penguins', 'Ducks', 'Senators', 'Blue Jackets', 'Panthers', 'Islanders'];
-    print('#5 : Liste des clubs avec les joueurs associés.');
-    print('Liste des joueurs : $playersName');
-    print('Liste des clubs : $clubsName');
-    print('Liste des clubs associés aux joueurs : ${playersClubs(playersName, clubsName)}');
-    print("");
- 
+  List playersName = [['Malkin', 'Crosby'], ['Souray', 'Perry'], ['Karlsson', 'Phillips'], ['Johnson', 'Dubinsky'], ['Luongo', 'Hayes'], ['Tavares', 'Vanek']];
+  List clubsName = ['Penguins', 'Ducks', 'Senators', 'Blue Jackets', 'Panthers', 'Islanders'];
+  print('#5 : Liste des clubs avec les joueurs associés.');
+  print('Liste des joueurs : $playersName');
+  print('Liste des clubs : $clubsName');
+  print('Liste des clubs associés aux joueurs : ${playersClubs(playersName, clubsName)}');
+  print("");
 }
 
  
--- bin/functions_ex06.dart
+++ bin/functions_ex06.dart
@@ -1,7 +1,6 @@
 library functions_ex06;
+
 //no.1
-
-
 bool palindrome(var mot) {
   for (int i = 0; i < mot.length ~/ 2; i++) {
     if (mot[i] == mot[mot.length - (1 + i)]) return true;
@@ -9,76 +8,72 @@ bool palindrome(var mot) {
   return false;
 }
 
-
-
-
 // no. 2
-
-differenceDates(var today, var anotherDate){
-var difference = today.difference(anotherDate);
-print ('Question 2: La différence est de ${difference.inDays} jours');
+differenceDates(var today, var anotherDate) {
+  var difference = today.difference(anotherDate);
+  print ('Question 2: La différence est de ${difference.inDays} jours');
 }
 
 // no. 3
-notation (num note) {
+notation(num note) {
   var lettre = "";
   if (note >= 95) {
-   		return lettre += "A+";
-   } else if (note <= 95 && note > 91.49) {
-   	  return lettre += "A";
-   } else if (note <= 91.5 && note > 87.99) {
-   		 return lettre += "A-";
-   } else if (note <= 88 && note > 84.49) {
-   	  return lettre += "B+";
-   } else if (note <= 84.5 && note > 80.99) {
-    	 return lettre += "B";
-   } else if (note <= 81 && note > 77.49) {
-     return lettre += "B-";
-   } else if (note <= 77.5 && note > 73.99) {
-     	return lettre += "C+";
-   } else if (note <= 74 && note > 70.49) {
-   	  return lettre += "C";
-   } else if (note <= 70.5 && note > 66.99) {
-    	 return lettre += "C-";
-   } else if (note <= 67 && note > 63.49) {
-    	 return lettre += "D+";
-   } else if (note <= 63.5 && note > 59.99) {
-    	 return lettre += "D";
-   } else if (note <= 60 && note > 0) {
-    	 return lettre += "E";
+    return lettre += "A+";
+  } else if (note <= 95 && note > 91.49) {
+   	return lettre += "A";
+  } else if (note <= 91.5 && note > 87.99) {
+   	return lettre += "A-";
+  } else if (note <= 88 && note > 84.49) {
+   	return lettre += "B+";
+  } else if (note <= 84.5 && note > 80.99) {
+    return lettre += "B";
+  } else if (note <= 81 && note > 77.49) {
+    return lettre += "B-";
+  } else if (note <= 77.5 && note > 73.99) {
+    return lettre += "C+";
+  } else if (note <= 74 && note > 70.49) {
+   	return lettre += "C";
+  } else if (note <= 70.5 && note > 66.99) {
+    return lettre += "C-";
+  } else if (note <= 67 && note > 63.49) {
+    return lettre += "D+";
+  } else if (note <= 63.5 && note > 59.99) {
+    return lettre += "D";
+  } else if (note <= 60 && note > 0) {
+    return lettre += "E";
   }
+}
 
 //No. 4
-  
-  List getThreeLists(List l) {
+List getThreeLists(List l) {
   List moinsquehuit = new List();
   List plusquehuit = new List();
   List egalahuit = new List();
   List returnList = new List();
   for (int i = 0; i < l.length; i++) {
-  String s = l.elementAt(i);
-  if (s.length < 8) {
-  moinsquehuit.add(s);
-  } else if (s.length > 8) {
-  plusquehuit.add(s);
-  } else {
-  egalahuit.add(s);
-  }
+    String s = l.elementAt(i);
+    if (s.length < 8) {
+      moinsquehuit.add(s);
+    } else if (s.length > 8) {
+      plusquehuit.add(s);
+    } else {
+      egalahuit.add(s);
+    }
   }
   returnList.add(moinsquehuit);
   returnList.add(egalahuit);
   returnList.add(plusquehuit);
   return returnList;
-  }
-  
+}
+ 
 //no. 5
 //Source : https://github.com/etdeschenes/ex06 
 Map playersClubs(List playersName, List clubsName) {
-var map = new Map();
-for (var i = 0; i < 6; i++) {
-  map [clubsName[i]] = playersName[i];
-}
-return map;
+  var map = new Map();
+  for (var i = 0; i < 6; i++) {
+    map [clubsName[i]] = playersName[i];
+  }
+  return map;
 }
   
-}
+
